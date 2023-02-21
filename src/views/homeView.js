56a71834ae92bd27f34ba83bcec06fe23612b6f4
Diego B. Couto <diiegao_@hotmail.com>
2023-02-21 12:07:12 -0300

    Add files via upload

--- src/views/homeView.js
+++ src/views/homeView.js
@@ -5,7 +5,9 @@ const template = /*html*/`
     <side-bar></side-bar>
     <div class="main-content" style="justify-content: flex-start; align-items: flex-start;">
         <h1>Aprenda como usar o Discord Profile</h1>
-        <iframe width="800" height="500" src="https://www.youtube.com/embed/2AkI6kdIvN8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div  style="width: 100%; display: flex; justify-content: center; align-items: center;">
+            <iframe width="800" height="500" src="https://www.youtube.com/embed/2AkI6kdIvN8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     </div>
 `
 
